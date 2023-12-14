d07eb526c0ede704306314b9e6d3b4078ca73545
jmz <292648912@qq.com>
2023-12-14 23:42:39 +0800

    add backup

---
+++ Assets/CatLikeBackups/Object Variety/Scripts/Shape.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Shape : PersistableObject {
+
+	static int colorPropertyId = Shader.PropertyToID("_Color");
+
+	static MaterialPropertyBlock sharedPropertyBlock;
+
+	public int MaterialId { get; private set; }
+
+	public int ShapeId {
+		get {
+			return shapeId;
+		}
+		set {
+			if (shapeId == int.MinValue && value != int.MinValue) {
+				shapeId = value;
+			}
+			else {
+				Debug.LogError("Not allowed to change ShapeId.");
+			}
+		}
+	}
+
+	int shapeId = int.MinValue;
+
+	Color color;
+
+	MeshRenderer meshRenderer;
+
+
+	void Awake () {
+		meshRenderer = GetComponent<MeshRenderer>();
+	}
+
+	public void SetColor (Color color) {
+		this.color = color;
+		if (sharedPropertyBlock == null) {
+			sharedPropertyBlock = new MaterialPropertyBlock();
+		}
+		sharedPropertyBlock.SetColor(colorPropertyId, color);
+		meshRenderer.SetPropertyBlock(sharedPropertyBlock);
+	}
+
+	public void SetMaterial (Material material, int materialId) {
+		meshRenderer.material = material;
+		MaterialId = materialId;
+	}
+
+	public override void Save (GameDataWriter writer) {
+		base.Save(writer);
+		writer.Write(color);
+	}
+
+	public override void Load (GameDataReader reader) {
+		base.Load(reader);
+		SetColor(reader.Version > 0 ? reader.ReadColor() : Color.white);
+	}
+}
